5e0d5a2c02b18535e13447e96c1d6f7e56258de4
Carlos Henrique Trindade <119907560+carloshenrique44@users.noreply.github.com>
2025-04-25 13:39:23 -0300

    App.py

--- APIAutenticação/App.py
+++ APIAutenticação/App.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'minha_chave_secreta'
+app.config['JWT_SECRET_KEY'] = 'my_secret_key'  
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
@@ -15,24 +15,20 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    
-    @app.before_first_request
-    def criar_banco():
-        db.create_all()
 
-app.config["JWT_SECRET_KEY"] = "sua_chave_secreta"
-jwt = JWTManager(app)  
-
-users = {"admin": "1234"}
+@app.before_first_request
+def criar_banco():
+    db.create_all()
 
 @app.route("/login", methods=["POST"])
 def login():
-
     dados = request.json
     username = dados.get("username")
     password = dados.get("password")
 
-    if users.get(username) == password:
+    usuario = Usuario.query.filter_by(username=username).first()
+
+    if usuario and usuario.password == password:
 
         token = create_access_token(identity=username)
         return jsonify({"access_token": token})
