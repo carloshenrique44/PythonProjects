44c44af05d4b7e210c2fff315b8d65e975164114
Carlos Henrique Trindade <carlos.trindade@ablsystem.dc.local>
2025-04-28 09:47:21 -0300

    CRUD com FASTAPI - primeira versão

--- DjangoCadastros/projeto_cadastro_django/projeto_cadastro_django/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from app_cadastro import views
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    
-    path('usuarios/', views.usuarios, name='listagem_usuarios')
-]
